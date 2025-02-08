649693222eac88c6fe4e1fb61d5bcec213a459c7
Ray Ramirez <rramirez70@gatech.edu>
2025-02-07 22:36:51 -0500

    chapter two
    Finished chapter two.

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'moviesstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
