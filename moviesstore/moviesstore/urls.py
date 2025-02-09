30d0f8819beca1e58b9750a7ed1057b5dd2f43d9
Ray Ramirez <rramirez70@gatech.edu>
2025-02-09 00:51:08 -0500

    Chapter 5
    Progress up to chapter 5.

--- moviesstore/moviesstore/urls.py
+++ moviesstore/moviesstore/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from xml.etree.ElementInclude import include
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -25,3 +26,4 @@ urlpatterns = [
        path('', include('home.urls')),
        path('movies/', include('movies.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
